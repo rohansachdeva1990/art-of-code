2fc00e5fc64ea5d052bddb8099185aa36415231f
rohansachdeva1990 <rohan.sachdeva1990@gmail.com>
2022-05-07 23:49:34 +0200

    [refactoring-kata-one] legacy code

---
+++ refactoring/kata-one/src/main/java/com/rohan/aoc/refactoring/kataone/EstateMaterial.java
@@ -0,0 +1,9 @@
+package com.rohan.aoc.refactoring.kataone;
+
+/**
+ * Copyright (c) 2020 IT Train Wlodzimierz Krakowski (www.refactoring.pl)
+ * Sources are available only for personal usage by Udemy students of this course.
+ */
+public enum EstateMaterial {
+    BRICK, WOOD, FERROCONCRETE, STONE
+}
